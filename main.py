124e8ec78fa86d7337719df501531cfa129185dd
brianfunk3 <brianfunk3.0@gmail.com>
2019-12-26 21:25:27 -0500

    trying to identify boards only

--- main.py
+++ main.py
@@ -9,36 +9,68 @@ Testing space. Goal is to find an appropriate way to find a rectangle on screen 
 1. Find rectangles with OpenCV - need to get corner points
 2. Abstract that space into a 2:1 ratio rectangle - might need to optimize points (or do this step within rectangle
    detection
-3. Average pool into a 20x10 matrix.
+3. Average pool into a 20x10 matrix. OR std dev pooling to find the consistency in each square? Do a threshhold? Hmmm
 4. Find error across clustering and use some threshold for making classification. Actually I might just want to get some
    error number to use and have classification made in main loop.
    
 helpful? : https://stackoverflow.com/questions/7263621/how-to-find-corners-on-a-image-using-opencv
 """
-#read in screen as a series of frames
+ratio_error = .01
 
-def screen_record(timetrial = False):
+def tetrisy(c):
+    return (1-ratio_error) <= (c[0][0]/c[3][0]) <= (1+ratio_error) and (1-ratio_error) <= (c[1][0]/c[2][0]) <= (1+ratio_error) \
+        and (1-ratio_error) <= (c[0][1]/c[3][1]) <= (1+ratio_error) and (1-ratio_error) <= (c[1][1]/c[2][1]) <= (1+ratio_error) #\
+        #and abs(c[1][1]-c[2][1])*(1-ratio_error) <= abs(c[0][0]-c[1][0])*2 <= abs(c[1][1]-c[2][1])*(1+ratio_error)
+
+def screen_record(timetrial = False, capture='screen', show=False):
     if timetrial:
         last_time = time.time()
-    sct = mss.mss()
+    if capture == 'screen':
+        sct = mss.mss()
+    elif capture == 'cam':
+        camera = cv2.VideoCapture(0)
+
     while(True):
-        sct_img = sct.grab(sct.monitors[1])
-        printscreen = np.array(Image.frombytes('RGB', sct_img.size, sct_img.bgra, 'raw', 'BGRX'))
-        # convert to grayscale
+        if capture == 'screen':
+            monitor = {"top": 0, "left": 0, "width": 950, "height": 1080}
+            sct_img = sct.grab(monitor)
+            printscreen = np.array(sct_img)
+        elif capture == 'cam':
+            printscreen = camera.read()[1]
+        # convert image to grayscale
         gray = cv2.cvtColor(printscreen, cv2.COLOR_BGR2GRAY)
+        # smooth the image
+        #smooth = cv2.GaussianBlur(gray,(5,5),0)
+        # edge detection
+        edges = cv2.Canny(gray,30,150)
+        # threshold the image - actually a bad idea lol
+        thresh = cv2.threshold(edges,225,255,cv2.THRESH_BINARY_INV)[1]
+        # find contours
+        contours, hierarchy = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        # approximate the polys (if existent)?
+        if len(contours) > 0:
+            for c in contours:
+                # uh
+                peri = cv2.arcLength(c, True)
+                poly = cv2.approxPolyDP(c,.04 * peri,True)
+                if len(poly) == 4 and cv2.contourArea(poly) > 1000:
+                    # polygon has 4 vertices, now need to check if it is tetris-y?
+                    poly = poly.reshape(-1,2)
+                    if tetrisy(poly):
+                        print("hmm")
+                        cv2.drawContours(printscreen, [c], -1, (0, 255, 0), 3)
+
         if timetrial:
             print('loop took {} seconds'.format(time.time()-last_time))
             last_time = time.time()
 
         # show the screen
-        """
-        cv2.imshow('testing things', gray)
-        
-        if cv2.waitKey(25) & 0xFF == ord('q'):
-            cv2.destroyAllWindows()
-            break
-        """
+        if show:
+            cv2.imshow('testing things', printscreen)
+
+            if cv2.waitKey(25) & 0xFF == ord('q'):
+                cv2.destroyAllWindows()
+                break
 
 
-
-screen_record(timetrial=True)
+screen_record(show=True)
