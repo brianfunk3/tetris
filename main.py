6d42a23972538df0d3a437a60b3abccafa1e169e
brianfunk3 <brianfunk3.0@gmail.com>
2020-01-09 21:25:13 -0500

    changed things?

--- main.py
+++ main.py
@@ -18,9 +18,12 @@ helpful? : https://stackoverflow.com/questions/7263621/how-to-find-corners-on-a-
 ratio_error = .01
 
 def tetrisy(c):
+    return True
+    """
     return (1-ratio_error) <= (c[0][0]/c[3][0]) <= (1+ratio_error) and (1-ratio_error) <= (c[1][0]/c[2][0]) <= (1+ratio_error) \
         and (1-ratio_error) <= (c[0][1]/c[3][1]) <= (1+ratio_error) and (1-ratio_error) <= (c[1][1]/c[2][1]) <= (1+ratio_error) #\
         #and abs(c[1][1]-c[2][1])*(1-ratio_error) <= abs(c[0][0]-c[1][0])*2 <= abs(c[1][1]-c[2][1])*(1+ratio_error)
+    """
 
 def screen_record(timetrial = False, capture='screen', show=False):
     if timetrial:
@@ -41,10 +44,12 @@ def screen_record(timetrial = False, capture='screen', show=False):
         gray = cv2.cvtColor(printscreen, cv2.COLOR_BGR2GRAY)
         # smooth the image
         #smooth = cv2.GaussianBlur(gray,(5,5),0)
-        # edge detection
-        edges = cv2.Canny(gray,30,150)
         # threshold the image - actually a bad idea lol
-        thresh = cv2.threshold(edges,225,255,cv2.THRESH_BINARY_INV)[1]
+        #thresh = cv2.threshold(gray,160,255,cv2.THRESH_BINARY)[1]
+        thresh = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
+            cv2.THRESH_BINARY,11,2)
+        # edge detection
+        edges = cv2.Canny(thresh,100,200)
         # find contours
         contours, hierarchy = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
         # approximate the polys (if existent)?
@@ -53,11 +58,11 @@ def screen_record(timetrial = False, capture='screen', show=False):
                 # uh
                 peri = cv2.arcLength(c, True)
                 poly = cv2.approxPolyDP(c,.04 * peri,True)
-                if len(poly) == 4 and cv2.contourArea(poly) > 1000:
+                if len(poly) == 4 and cv2.contourArea(poly) > 3000:
                     # polygon has 4 vertices, now need to check if it is tetris-y?
                     poly = poly.reshape(-1,2)
                     if tetrisy(poly):
-                        print("hmm")
+                        #print("hmm")
                         cv2.drawContours(printscreen, [c], -1, (0, 255, 0), 3)
 
         if timetrial:
@@ -66,7 +71,7 @@ def screen_record(timetrial = False, capture='screen', show=False):
 
         # show the screen
         if show:
-            cv2.imshow('testing things', printscreen)
+            cv2.imshow('testing things', thresh)
 
             if cv2.waitKey(25) & 0xFF == ord('q'):
                 cv2.destroyAllWindows()
